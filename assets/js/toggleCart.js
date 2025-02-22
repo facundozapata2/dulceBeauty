afe68720012c026a4ce4896d49eed7edaeb8c7fa
facundozapata2 <facundozapata2@gmail.com>
2025-02-21 23:55:33 -0300

    css transition

--- assets/js/toggleCart.js
+++ assets/js/toggleCart.js
@@ -1,19 +1,22 @@
-// 📁 js/toggleCart.js
 export function toggleCart() {
-    const cart = document.querySelector('.shopping-card__sidebar');
-    const cartBtn = document.getElementById('shoppingCartBtn');
-  
-    if (!cart || !cartBtn) return;
-    
-    cartBtn.addEventListener('click', () => {
-      cart.classList.toggle('cart-open');
-      cart.classList.toggle('cart-hidden');
-    });
-  
-    document.addEventListener('click', (e) => {
-      if (!cart.contains(e.target) && !cartBtn.contains(e.target)) {
-        cart.classList.remove('cart-open');
-        cart.classList.add('cart-hidden');
-      }
-    });
+  const cart = document.querySelector('.shopping-card__sidebar');
+  const cartBtn = document.getElementById('shoppingCartBtn');
+
+  // Verificar que existe la clase inicial
+  if (!cart.classList.contains('cart-hidden')) {
+    cart.classList.add('cart-hidden');
   }
+
+  cartBtn.addEventListener('click', () => {
+    cart.classList.toggle('cart-hidden');
+    cart.classList.toggle('cart-open');
+  });
+
+  // Cierre al hacer click fuera
+  document.addEventListener('click', (e) => {
+    if (!cart.contains(e.target) && !cartBtn.contains(e.target)) {
+      cart.classList.remove('cart-open');
+      cart.classList.add('cart-hidden');
+    }
+  });
+}
